47d392d562ad8385f4d3b5e65fa2e6dad42132aa
SamuelCelisL <samuelcelislizcano@gmail.com>
2024-10-22 14:08:33 -0500

    Se agregaron Comentarios y estilos basicos a los JCheckBox

--- src/opciones/paneldeOpciones.java
+++ src/opciones/paneldeOpciones.java
@@ -1,6 +1,7 @@
 package opciones;
 
 import javax.swing.*;
+
 import java.awt.*;
 import javax.swing.border.Border;
 // import java.awt.event.ActionListener;
@@ -9,31 +10,61 @@ import javax.swing.border.Border;
 public class paneldeOpciones extends JPanel {
 
     public JPanel menu() {
-        JPanel panelmenu = new JPanel();
-        Border borde = BorderFactory.createLineBorder(Color.BLACK, 2);
 
-        panelmenu.setBounds(550, 50, 200, 80);
+        // Creación y customización del panel
+        JPanel panelmenu = new JPanel();
+        Border borde = BorderFactory.createLineBorder(Color.BLACK, 5);
+        panelmenu.setBackground(new Color(0xD3D3D3));
+        panelmenu.setLayout(null);
+
+        // Ubicacion del panel en la vetana principal
+        int ubicacionX = 450;
+        int ubicacionY = 50;
+        panelmenu.setBounds(ubicacionX, ubicacionY, 300, 100);
+
+        ButtonGroup comunicacion = new ButtonGroup();
+        // Creacion de los elementos del menu
         JButton botonInicio = new JButton("Inicio");
         JButton botonFinalizar = new JButton("Finalizar");
         JCheckBox automatizado = new JCheckBox("Autimatico");
         JCheckBox manual = new JCheckBox("Manual");
 
-        automatizado.setBounds(20, 50, 70, 20);
+        comunicacion.add(automatizado);
+        comunicacion.add(manual);
+
+        // ! Diseño de los CheckBox
+        automatizado.setFont(new Font("Arial", Font.BOLD, 14));
+        automatizado.setForeground(new Color(0x000000));
+        // automatizado.setBackground(new Color(0x162524));
+        automatizado.setFocusPainted(false);
+        automatizado.setOpaque(false);
+
+        manual.setFont(new Font("Arial", Font.BOLD, 14));
+        manual.setForeground(new Color(0x000000));
+        // manual.setBackground(new Color(0x162524));
+        manual.setFocusPainted(false);
+        manual.setOpaque(false);
+
+        // ubicación de los checkBox del menu
+        int alturaestados = 60;
+        automatizado.setBounds(40, alturaestados, 100, 20);
         automatizado.setFocusable(false);
-        manual.setBounds(90, 50, 60, 20);
+        manual.setBounds(160, alturaestados, 100, 20);
         manual.setFocusable(false);
 
-        botonInicio.setBounds(10, 10, 80, 20);
+        // Ubicación de los botones de inicio y finalizacion
+        int alturabotones = 20;
+        botonInicio.setBounds(40, alturabotones, 80, 20);
         botonInicio.setFocusable(false);
-        botonFinalizar.setBounds(10, 40, 80, 20);
+        botonFinalizar.setBounds(160, alturabotones, 100, 20);
         botonFinalizar.setFocusable(false);
 
-        panelmenu.setBackground(new Color(0xD3D3D3));
-
+        // Implementación de los elementos al panel
         panelmenu.add(botonInicio);
         panelmenu.add(botonFinalizar);
         panelmenu.add(automatizado);
         panelmenu.add(manual);
+        // panelmenu.add(comunicacion);
 
         panelmenu.setBorder(borde);
         return panelmenu;
