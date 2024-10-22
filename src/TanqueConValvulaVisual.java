7bb584999e8304972299396d1914b4c0ad434c5f
SamuelCelisL <samuelcelislizcano@gmail.com>
2024-10-21 22:23:12 -0500

    Se agregaron los CheckButton para el modo manual y automatico (solo el Front-End)

--- src/TanqueConValvulaVisual.java
+++ src/TanqueConValvulaVisual.java
@@ -33,20 +33,28 @@ public class TanqueConValvulaVisual extends JPanel implements ActionListener {
         JPanel panelmenu = new JPanel();
         Border borde = BorderFactory.createLineBorder(Color.BLACK, 2);
 
-        panelmenu.setBounds(500, 50, 200, 80);
+        panelmenu.setBounds(550, 50, 200, 80);
         JButton botonInicio = new JButton("Inicio");
         JButton botonFinalizar = new JButton("Finalizar");
+        JCheckBox automatizado = new JCheckBox("Autimatico");
+        JCheckBox manual = new JCheckBox("Manual");
+
+        automatizado.setBounds(20, 50, 70, 20);
+        automatizado.setFocusable(false);
+        manual.setBounds(90, 50, 60, 20);
+        manual.setFocusable(false);
 
         botonInicio.setBounds(10, 10, 80, 20);
         botonInicio.setFocusable(false);
         botonFinalizar.setBounds(10, 40, 80, 20);
         botonFinalizar.setFocusable(false);
-        ;
 
         panelmenu.setBackground(new Color(0xD3D3D3));
 
         panelmenu.add(botonInicio);
         panelmenu.add(botonFinalizar);
+        panelmenu.add(automatizado);
+        panelmenu.add(manual);
 
         panelmenu.setBorder(borde);
         this.add(panelmenu);
